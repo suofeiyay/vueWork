3cb30d085ec90247c67118930eb8d2a00d45192b
suofeiyay <824994265@qq.com>
2019-11-29 16:54:55 +0800

    updata

---
+++ ebook/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ul>
+      <li class="txt">1</li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+
+  .txt{
+    font-size: px2rem(20)
+  }
+
+</style>
